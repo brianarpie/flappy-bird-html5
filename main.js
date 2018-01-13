f6197d2ed6f0242383fd741d361f6e8f127a0b0d
brianarpie <brian.arpie@gmail.com>
2018-01-13 13:36:12 -0500

    build flappy bird html5 tutorial complete

---
+++ main.js
@@ -0,0 +1,75 @@
+var game = new Phaser.Game(400, 490);
+
+var mainState = {
+  preload: function() {
+    game.load.image('bird', 'assets/bird.png');
+    game.load.image('pipe', 'assets/pipe.png');
+    game.load.audio('jump', 'assets/jump.wav');
+  },
+  create: function() {
+    game.stage.backgroundColor = '#71c5cf';
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    this.bird = game.add.sprite(100, 245, 'bird');
+    game.physics.arcade.enable(this.bird);
+    this.bird.body.gravity.y = 1000;
+    var spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    spaceKey.onDown.add(this.jump, this);
+    this.pipes = game.add.group();
+    this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+    this.score = 0;
+    this.labelScore = game.add.text(20, 20, "0",
+      { font: "30px Arial", fill: "#fff" });
+    this.bird.anchor.setTo(-0.2, 0.5);
+    this.jumpSound = game.add.audio('jump');
+  },
+  update: function() {
+    game.physics.arcade.overlap(this.bird, this.pipes, this.hitPipe, null, this);
+    if (this.bird.y < 0 || this.bird.y > 490)
+      this.restartGame();
+    if (this.bird.angle < 20)
+      this.bird.angle += 1;
+  },
+  hitPipe: function() {
+    if (this.bird.alive == false)
+      return;
+    this.bird.alive = false;
+    game.time.events.remove(this.timer);
+    this.pipes.forEach(function(p) {
+      p.body.velocity.x = 0;
+    }, this);
+  },
+  addOnePipe: function(x, y) {
+    var pipe = game.add.sprite(x, y, 'pipe');
+    this.pipes.add(pipe);
+    game.physics.arcade.enable(pipe);
+    pipe.body.velocity.x = -200;
+    pipe.checkWoldBounds = true;
+    pipe.outOfBoundsKill = true;
+  },
+  addRowOfPipes: function() {
+    this.score +=1 ;
+    this.labelScore.text = this.score;
+
+    var holePosition = Math.floor(Math.random() * 5) + 1;
+    for (var i = 0; i < 8; i++)
+      if (i !== holePosition && i !== holePosition + 1)
+        this.addOnePipe(400, i * 60 + 10);
+
+  },
+  jump: function() {
+    if (this.bird.alive == false)
+      return;
+    this.jumpSound.play();
+    this.bird.body.velocity.y = -350;
+    var animation = game.add.tween(this.bird);
+    animation.to({ angle: -20 }, 100);
+    animation.start();
+  },
+  restartGame: function() {
+    game.state.start('main');
+  }
+};
+
+game.state.add('main', mainState);
+
+game.state.start('main');
